570d883d055987b29c827195bcd905fd2a9f3b7d
viktorHek <viktorkarlsson125@gmail.com>
2022-11-10 10:26:44 +0100

    done ich. still finde small errors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function newGame() {
     let box = document.getElementById(`box-${index}`);
     box.remove();
   }
-  result.innerHTML = 0
+  result.innerHTML = 0;
   populateGame();
 }
 function select(index) {
@@ -16,7 +16,7 @@ function select(index) {
     console.log("shit");
   }
   if (box.innerHTML === "x") {
-    if(result.innerHTML !== "0") {
+    if (result.innerHTML !== "0") {
       result.innerHTML = "You lose";
     }
   } else {
@@ -47,9 +47,7 @@ function populateGame() {
     bombArr.push(number);
   }
   let bombPosIndex = bombArr;
-
   let numberPos = getNumberPos(boxContentArr, bombPosIndex);
-
   for (let index = 0; index < amoutOfBoxes; index++) {
     if (bombPosIndex.includes(index)) {
       let box = document.getElementById(`box-${index}`);
@@ -64,76 +62,46 @@ function populateGame() {
   }
 }
 function getNumberPos(boxContentArr, bombPosIndex) {
-  console.log("hej: ", boxContentArr, bombPosIndex);
   let newArr = boxContentArr;
-  // const topLine = [0, 1, 2, 3, 4, 5, 6, 7];
-  // const rightLine = [7, 15, 23, 31, 39, 47, 55, 63];
-  // const leftLine = [0, 8, 16, 24, 32, 40, 48, 56];
-  // const bottomLine = [56, 57, 58, 59, 60, 61, 62, 63];
-  // const borderPos = [0, 1, 2, 3, 4, 5, 6, 7, 15, 23, 31, 39, 47, 55, 63, 8, 16, 24, 32, 40, 48, 56, 57, 58, 59, 60, 61, 62, 63]
+  const topLine = [0, 1, 2, 3, 4, 5, 6, 7];
+  const bottomLine = [56, 57, 58, 59, 60, 61, 62, 63];
+  const rightLine = [7, 15, 23, 31, 39, 47, 55, 63];
+  const leftLine = [0, 8, 16, 24, 32, 40, 48, 56];
   for (let index = 0; index < bombPosIndex.length; index++) {
     const indexOfBomb = bombPosIndex[index];
-    // if(index === 10) {
-    //   console.log('included in rightLine: ', rightLine.includes(index))
-    //   console.log('included in leftLine: ', leftLine.includes(index))
-    //   console.log('included in topLine: ', topLine.includes(index))
-    //   console.log('included in bottomLine: ', bottomLine.includes(index))
-    // }
-    // if(rightLine.includes(index)) {
-    //   newArr[indexOfBomb - 1] = newArr[indexOfBomb - 1] + 1; // left
-    //   if(topLine.includes(index)) {
-    //     newArr[indexOfBomb + 8] = newArr[indexOfBomb + 8] + 1; // bottom
-    //     newArr[indexOfBomb + 7] = newArr[indexOfBomb + 7] + 1; // bottom left            
-    //   }
-    //   if(bottomLine.includes(index)) {
-    //     newArr[indexOfBomb - 8] = newArr[indexOfBomb - 8] + 1; // top
-    //     newArr[indexOfBomb - 7] = newArr[indexOfBomb - 7] + 1; // top left  
-    //   }
-    // }
-    // if(topLine.includes(index)) {
-    //   newArr[indexOfBomb + 1] = newArr[indexOfBomb + 1] + 1; // right
-    //   if(rightLine.includes(index)) {
-    //     newArr[indexOfBomb - 1] = newArr[indexOfBomb - 1] + 1; // left
-    //     newArr[indexOfBomb + 7] = newArr[indexOfBomb + 7] + 1; // bottom left       
-    //   }
-    //   if(leftLine.includes(index)) {
-    //     newArr[indexOfBomb + 8] = newArr[indexOfBomb + 8] + 1; // bottom
-    //     newArr[indexOfBomb + 9] = newArr[indexOfBomb + 9] + 1; // bottom right  
-    //   }
-    // }
-    // if(leftLine.includes(index)) {
-    //   newArr[indexOfBomb + 1] = newArr[indexOfBomb + 1] + 1; // right
-    //   if(topLine.includes(index)) {
-    //     newArr[indexOfBomb + 8] = newArr[indexOfBomb + 8] + 1; // bottom
-    //     newArr[indexOfBomb + 9] = newArr[indexOfBomb + 9] + 1; // bottom right
-    //   }
-    //   if(bottomLine.includes(index)) {
-    //     newArr[indexOfBomb - 8] = newArr[indexOfBomb - 8] + 1; // top
-    //     newArr[indexOfBomb - 9] = newArr[indexOfBomb - 9] + 1; // top right  
-    //   }
-    // }
-    // if(bottomLine.includes(index)) {
-    //   newArr[indexOfBomb - 8] = newArr[indexOfBomb - 8] + 1; // top
-    //   if(rightLine.includes(index)) {
-    //     newArr[indexOfBomb - 1] = newArr[indexOfBomb - 1] + 1; // left
-    //     newArr[indexOfBomb - 7] = newArr[indexOfBomb - 7] + 1; // top left
-    //   }
-    //   if(leftLine.includes(index)) {
-    //     newArr[indexOfBomb + 1] = newArr[indexOfBomb + 1] + 1; // right
-    //     newArr[indexOfBomb - 9] = newArr[indexOfBomb - 9] + 1; // top right
-    //   }
-    // }
-    // if(borderPos.includes(index) === false) {
-    //   console.log('center: ',index)
-    // }
     newArr[indexOfBomb + 1] = newArr[indexOfBomb + 1] + 1; // right
     newArr[indexOfBomb - 1] = newArr[indexOfBomb - 1] + 1; // left
     newArr[indexOfBomb + 8] = newArr[indexOfBomb + 8] + 1; // bottom
     newArr[indexOfBomb - 8] = newArr[indexOfBomb - 8] + 1; // top
     newArr[indexOfBomb + 9] = newArr[indexOfBomb + 9] + 1; // bottom right
-    newArr[indexOfBomb - 9] = newArr[indexOfBomb - 9] + 1; // top right
-    newArr[indexOfBomb - 7] = newArr[indexOfBomb - 7] + 1; // top left
+    newArr[indexOfBomb - 9] = newArr[indexOfBomb - 9] + 1; // top left
     newArr[indexOfBomb + 7] = newArr[indexOfBomb + 7] + 1; // bottom left
+    newArr[indexOfBomb - 7] = newArr[indexOfBomb - 7] + 1; // top right
+    if (topLine.includes(indexOfBomb)) {
+      newArr[indexOfBomb - 8] = newArr[indexOfBomb - 8] - 1; // top
+      newArr[indexOfBomb - 7] = newArr[indexOfBomb - 7] - 1; // top right
+      newArr[indexOfBomb - 9] = newArr[indexOfBomb - 9] - 1; // top left
+    }
+    if (bottomLine.includes(indexOfBomb)) {
+      newArr[indexOfBomb + 8] = newArr[indexOfBomb + 8] - 1; // bottom
+      newArr[indexOfBomb + 9] = newArr[indexOfBomb + 9] - 1; // bottom right
+      newArr[indexOfBomb + 7] = newArr[indexOfBomb + 7] - 1; // bottom left
+    }
+    if (rightLine.includes(indexOfBomb)) {
+      newArr[indexOfBomb + 1] = newArr[indexOfBomb + 1] - 1; // right
+      newArr[indexOfBomb + 9] = newArr[indexOfBomb + 9] - 1; // bottom right
+      newArr[indexOfBomb - 7] = newArr[indexOfBomb - 7] - 1; // top right
+    }
+    if (leftLine.includes(indexOfBomb)) {
+      newArr[indexOfBomb - 1] = newArr[indexOfBomb - 1] - 1; // left
+      newArr[indexOfBomb - 9] = newArr[indexOfBomb - 9] - 1; // top left
+      newArr[indexOfBomb + 7] = newArr[indexOfBomb + 7] - 1; // bottom left
+    }
+  }
+  for (let index = 0; index < newArr.length; index++) {
+    if(newArr[index] < 0) {
+      newArr[index] = 0
+    }
   }
   return newArr;
 }
